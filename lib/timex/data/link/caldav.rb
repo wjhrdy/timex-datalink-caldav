df97d81d352dc5d53754601498f75dc65286e73a
Willy Hardy <whardy@redhat.com>
2023-05-11 13:34:23 -0400

    init

---
+++ lib/timex/data/link/caldav.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "caldav/version"
+
+module Timex
+  module Data
+    module Link
+      module Caldav
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/timex/data/link/caldav/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Timex
+  module Data
+    module Link
+      module Caldav
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
